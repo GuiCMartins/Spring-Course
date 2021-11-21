c2e3f49f954c43037bd00a43c2097d8e8115022b
Guilherme <guilhermecmartins@gmail.com>
2021-11-21 15:15:24 -0300

    OrdersAjusts

--- src/main/java/com/example/cursoSpring/entities/Order.java
+++ src/main/java/com/example/cursoSpring/entities/Order.java
@@ -12,15 +12,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd'T'hh:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -50,7 +55,7 @@ public class Order implements Serializable{
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
-	
+
 	public User getClient() {
 		return client;
 	}
@@ -75,7 +80,5 @@ public class Order implements Serializable{
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
+
 }
---
+++ src/main/java/com/example/cursoSpring/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.example.cursoSpring.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursoSpring.entities.Order;
+import com.example.cursoSpring.repositoires.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id){
+		return repository.findById(id).get();
+	}
+}
