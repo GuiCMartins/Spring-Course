cbc8492bd598f7fe70556e1d9c06f9ae6470d209
Guilherme <guilhermecmartins@gmail.com>
2021-11-22 18:44:18 -0300

    Database Error

--- src/main/java/com/example/cursoSpring/services/UserService.java
+++ src/main/java/com/example/cursoSpring/services/UserService.java
@@ -3,6 +3,8 @@ package com.example.cursoSpring.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.cursoSpring.entities.User;
@@ -28,7 +30,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new ResourceNotFoundException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User user) {
