a11a24abb16aba05f778472e8916fd086b2291df
Guilherme <guilhermecmartins@gmail.com>
2021-11-22 17:45:55 -0300

    update User

--- src/main/java/com/example/cursoSpring/resources/UserResource.java
+++ src/main/java/com/example/cursoSpring/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,17 +27,13 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		
 		List<User> list = service.findAll();
-		
 		return ResponseEntity.ok().body(list);
 	}
 	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
-		
 		User user = service.findById(id);
-		
 		return ResponseEntity.ok().body(user);
 	}
 	
@@ -51,5 +48,11 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
 	}
 }
--- src/main/java/com/example/cursoSpring/services/UserService.java
+++ src/main/java/com/example/cursoSpring/services/UserService.java
@@ -29,4 +29,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User user) {
+		User entity = repository.getById(id);
+		updateUser(entity, user);
+		return repository.save(entity);
+	}
+
+	private void updateUser(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
+	}
 }
